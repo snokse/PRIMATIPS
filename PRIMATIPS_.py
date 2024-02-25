95787fe061c40a9406b1a1a1d24c64c795ac1f44
snokse <snokse3@gmail.com>
2024-02-26 00:22:52 +0100

    CREER TABLE TKINTER

--- PRIMATIPS_.py
+++ PRIMATIPS_.py
@@ -3,8 +3,32 @@
 #Create label 
 
 from tkinter import *
+import mysql.connector
 import my_fonction
-   
+
+try:
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        passwd="",
+        database= "primatips_db"
+    )
+
+    mycursor = mydb.cursor()
+    sql_RES_1="SELECT COUNT(RF) FROM primatips_table WHERE BET_1 LIKE '1.7'"
+    sql_RES_X="SELECT RF FROM primatips_table WHERE BET_X LIKE '3.2'"
+    sql_RES_2="SELECT RF FROM primatips_table WHERE BET_2 LIKE '4.2'"
+    
+    mycursor.execute(sql_RES_1)
+    RES_1 = mycursor.fetchall()
+    #print("connected")
+
+    for RF in RES_1:
+        print(RF)
+    # mydb.commit()
+except mysql.connector.Error as r:
+    print(r)
+
 Mywindow=Tk()
 Mywindow.geometry("780x580")
 Mywindow.title("PRIMATIPS BET")
@@ -12,29 +36,6 @@ Mywindow.config(background="#165e8e")
 Mywindow.iconbitmap("IMG/PRIMATIPS_ICO.ico")
 
 
-"""
-frame=Frame(Mywindow, bg="#fff")
-
-Bet1=Label(frame,text="1", fg="#edf0f2", bg="#165e8e", font=("courrier",24))
-Bet1.pack()
-zone_1=Entry(frame)
-zone_1.pack()
-
-BetX=Label(frame,text="X", fg="#edf0f2", bg="#165e8e", font=("courrier",24))
-BetX.pack()
-zone_X=Entry(frame)
-zone_X.pack()
-
-Bet2=Label(frame,text="2", fg="#edf0f2", bg="#165e8e", font=("courrier",24))
-Bet2.pack()
-zone_2=Entry(frame)
-zone_2.pack()
-
-But_Valide=Button(frame,text="Validé", command=Valide)
-But_Valide.pack()
-
-frame.pack(expand=YES)
-"""
 # Label and Entry for Titre 
 Titre=Label(Mywindow,text="Welcome to PRIMATIPS BET", fg="#000", bg="#FFF", font=("courrier,52"))
 Titre.grid(row=0, columnspan=1, column=3)
@@ -66,9 +67,20 @@ Button(text="Afficher", command=my_fonction.afficher).grid(row=5, columnspan=1, 
 # Label to display the entered values
 frame=Frame(Mywindow, bg="#FFF" ,width=300, height=300)
 
-my_fonction.result_label = Label(frame, text="", font=("Arial", 12))
-my_fonction.result_label.grid(row=6, columnspan=1)
+Label(frame, text="1 - X - 2 - 1X2 - 1X - 12 - X2", fg="white", bg="#165e8e", font=("courier", 16)).grid(row=5, column=2)
+
+my_fonction.result_label1 = Label(frame, text="", font=("Arial", 12))
+my_fonction.result_label1.grid(row=6, columnspan=1)
+
+my_fonction.result_labelX = Label(frame, text="", font=("Arial", 12))
+my_fonction.result_labelX.grid(row=7, columnspan=1)
+
+my_fonction.result_label2 = Label(frame, text="", font=("Arial", 12))
+my_fonction.result_label2.grid(row=8, columnspan=1)
+
+
 
 frame.grid(row=6, column=2, columnspan=1)
+
 
 Mywindow.mainloop()
